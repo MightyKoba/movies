9d456a50f71c8d493f476ed1b7a19526aa713e40
Brandon Munce <brandonmunce@gmail.com>
2017-12-12 15:33:44 -0500

    brandon

--- manage.py
+++ manage.py
@@ -9,9 +9,11 @@ def deploy():
     db.drop_all()
     db.create_all()
     damon = Actor(name='Matt Damon', age=45)
+    titanic = Movie(title='Titanic', genre='Drama/Romance' , year=1999 , actor='Leonardo DiCaprio')
     db.session.add(damon)
+    db.session.add(titanic)
 
-db.session.commit()
+    db.session.commit()
 
 
 if __name__ == "__main__":
